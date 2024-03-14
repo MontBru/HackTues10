2fd5b189f04a595a49a991ac0ef5a1394f561432
Stefan Georgiev <tefcho.geo@gmail.com>
2024-03-14 05:15:50 +0200

    Login and SignIn UI ~ready

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,5 +1,32 @@
 import "@/styles/globals.css";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#D4D4D4',
+    },
+    background: {
+      default: '#121212',
+      paper: '#1E1E1E',
+    },
+    text: {
+      primary: '#FFFFFF',
+      secondary: '#B0B0B0',
+    },
+    action: {
+      hover: '#424242',
+    },
+  },
+});
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+  );
 }
