108f1755fc4eb660146fb404a9db00d703cdefa8
ItsRizee <101361202+ItsRizee@users.noreply.github.com>
2024-03-14 19:30:14 +0200

    Added teacher's side of home page

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,14 +1,21 @@
 import {useState} from "react";
 import {IconButton} from "@mui/material";
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import CircleIcon from '@mui/icons-material/Circle';
 import Link from "next/link";
 
-export default function Navbar() {
+export default function Navbar({isStudent}) {
     const [name, setName] = useState("Nikola Petrov");
 
     return (
         <nav className="flex items-center justify-between w-full h-20">
             <h1 className="text-3xl text-neutral-300">Beat box</h1>
+            {!isStudent &&
+                <div className="flex items-center space-x-1.5">
+                    <p className="text-2xl text-neutral-300">Live</p>
+                    <CircleIcon fontSize="small" color="error"/>
+                </div>
+            }
             <div className="flex items-center space-x-10">
                 <p className="text-xl text-neutral-300">{name}</p>
                 <Link href="/login">
--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -3,21 +3,61 @@ import Navbar from "@/components/Navbar";
 import LinearChart from "@/components/LinearChart";
 import DatePickerField from "@/components/DatePickerField";
 import BarChart from "@/components/BarChart";
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import React, {useState} from "react";
+import Grid from "@mui/material/Grid";
+import MetricsCard from "@/components/MetricsCard";
 
 const rubik = Rubik({ subsets: ["cyrillic"] });
 
 export default function Home() {
+  const [clas, setClas] = useState(null);
+  const isStudent = true;
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center bg-neutral-900 px-10 space-y-10 pb-20 ${rubik.className}`}
     >
-      <Navbar/>
+      <Navbar isStudent={isStudent}/>
       {/*<MetricsCard title="this month" attention="8/10"/>*/}
-      <div className="flex flex-col items-center justify-between w-full space-y-10 sm:px-80">
-        <DatePickerField/>
-        <LinearChart title={""} labels={['English', "Maths", "VOT", "IOT", "Biology", "Chemistry", "History"]} userData={[100, 75, 65, 90, 110, 130, 60]}/>
-        <BarChart title={""} labels={['English', "Maths", "VOT", "IOT", "Biology", "Chemistry", "History"]} userData={[5, 8, 6, 9, 7, 4, 5]}/>
-      </div>
+      { isStudent ?
+        <div className="flex flex-col items-center justify-between w-full space-y-10 sm:px-80">
+          <DatePickerField/>
+          <LinearChart title={""} labels={['English', "Maths", "VOT", "IOT", "Biology", "Chemistry", "History"]} userData={[100, 75, 65, 90, 110, 130, 60]}/>
+          <BarChart title={""} labels={['English', "Maths", "VOT", "IOT", "Biology", "Chemistry", "History"]} userData={[5, 8, 6, 9, 7, 4, 5]}/>
+        </div> :
+        <div className="flex flex-col space-y-5">
+          <div className="flex items-center w-60">
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Class</InputLabel>
+              <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={clas}
+                  label="Class"
+                  onChange={(newClass) => setClas(newClass)}
+              >
+                <MenuItem value={"12V"}>12V</MenuItem>
+                <MenuItem value={"11B"}>11B</MenuItem>
+                <MenuItem value={"10G"}>10G</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="flex flex-row">
+            <Grid container spacing={10}>
+              <Grid item>
+                <MetricsCard title="this month" attention={7}/>
+              </Grid>
+              <Grid item>
+                <MetricsCard title="this week" attention={6}/>
+              </Grid>
+              <Grid item>
+                <MetricsCard title="today" attention={7}/>
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+      }
     </main>
   );
 }
